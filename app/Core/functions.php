01c4ead6e8664666b936793ca2fca1293be5aaba
Egor Vasyakin <e.vasyakin@itevas.ru>
2022-10-14 18:20:18 +0300

    Update functions.php

--- app/Core/functions.php
+++ app/Core/functions.php
@@ -21,7 +21,7 @@ function env(string $name, $default = null) {
  * @param string|null тип
  */
 function debug($data, string $type = null) {
-    if (defined('DEBUG') && true === DEBUG) {
+    if (defined('DEBUG') && true == DEBUG) {
         echo '<div class="debug">' . (is_null($type) ? '' : "<b>{$type} debug:</b>") 
         . json_encode($data, JSON_UNESCAPED_UNICODE) . '</div>';
     }
