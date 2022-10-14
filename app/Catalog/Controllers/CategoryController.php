fadb6e292b880670a94f79d03f4bf0172dac5cd3
Egor Vasyakin <e.vasyakin@itevas.ru>
2022-10-14 04:28:42 +0300

    Catalog: create controllers

---
+++ app/Catalog/Controllers/CategoryController.php
@@ -0,0 +1,43 @@
+<?php
+namespace Catalog\Controllers;
+
+use Catalog\Services\CategoryService;
+use Catalog\Services\ProductService;
+use Core\App;
+use Core\Router\Controller;
+
+class CategoryController extends Controller
+{
+    public function list()
+    {
+        $cats = CategoryService::globalCategories();
+        $this->view('catalog/category.php', compact('cats'));
+    }
+
+    public function one(string $url)
+    {
+        // var_dump(App::request()->getProp('price'));
+        @[$cat, $cat_ids, $filters] = CategoryService::categoryByUrl($url);
+
+        // check filters
+        foreach ($filters as $filter) {
+            $filter_value = App::request()->getProp($filter->product_field);
+            if (!empty($filter_value)) {
+                $filter->value = explode(';', $filter_value);
+            }
+        }
+
+        $products = ProductService::productsList($cat_ids, $filters);
+
+        // $products = ProductService::productsByCategories($cat_ids);
+        $products2 = ProductService::productsByCategoriesWithDuplicates($cat_ids);
+        
+
+        $this->view('catalog/category.php', compact(
+            'cat', 'cat_ids', 'products', 'filters',
+            'products2'
+        ));
+        // echo $cat;
+        // echo json_encode($cat, JSON_UNESCAPED_UNICODE);
+    }
+}
